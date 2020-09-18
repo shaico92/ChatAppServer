f381f03c6db6ca7f5e2a5eabf2bb2c3480f10fbf
Shai Cohen <Shai.c@Hartech.local>
2020-09-17 17:51:08 -0700

    adding listening to clients

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const PORT =4000;
+const PORT = 4000;
 
-const io = require('socket.io')(PORT);
-console.log('server up')
-const users = {}
+const io = require("socket.io")(PORT);
+console.log("server up");
+const users = {};
 
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+    console.log(name);
+  });
 
-io.on('connection', socket=>{
-    
-    socket.on('new-user' , name=>{
-        
-        users[socket.id] = name;
-        socket.broadcast.emit('user-connected',name);
-        console.log(name)
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
     });
-    
-    socket.on('send-chat-message', message=>{
-        console.log(message);
-        
-        //socket.broadcast.emit('chat-message',{message: message , name : users[socket.id]} );
-        socket.emit('chat-message',message );
-    })
-
+    //socket.broadcast.emit("chat-message", message);
+  });
 });
 
-console.log(`Server runninh on port ${PORT}`)
+console.log(`Server runninh on port ${PORT}`);
