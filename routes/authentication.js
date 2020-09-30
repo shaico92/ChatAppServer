9bbcc5740ea817519d04e42c8514d5b3bc7a79df
Shai Cohen <Shai.c@Hartech.local>
2020-09-30 09:37:00 -0700

    fixing picture path

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,28 +2,28 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/Users");
-const path= require('path');
-const multer  = require('multer')
-var fileUrl = require('file-url');
-const upload = multer()
+const path = require("path");
+const multer = require("multer");
+var fileUrl = require("file-url");
+const upload = multer();
 
-const fs = require("fs"); 
+const fs = require("fs");
 const { promisify } = require("util");
-const pipeline = promisify(require('stream').pipeline)
+const pipeline = promisify(require("stream").pipeline);
 
-const fileUrlConverter=(str)=> {
-  if (typeof str !== 'string') {
-      throw new Error('Expected a string');
+const fileUrlConverter = (str) => {
+  if (typeof str !== "string") {
+    throw new Error("Expected a string");
   }
 
-  let pathName = path.resolve(str).replace(/\\/g, '/');
+  let pathName = path.resolve(str).replace(/\\/g, "/");
 
   // Windows drive letter must be prefixed with a slash
-  if (pathName[0] !== '/') {
-      pathName = '/' + pathName;
+  if (pathName[0] !== "/") {
+    pathName = "/" + pathName;
   }
 
-  return encodeURI('file://' + pathName);
+  return encodeURI("file://" + pathName);
 };
 
 router.use((req, res, next) => {
@@ -54,8 +54,13 @@ const checkUserCredMatch = (userEmail, userPass) => {
 };
 
 //adding user to db
-const addUserToDB = (userName, userPass, userEmail,userImage) => {
-  const user_ = { username: userName, password: userPass, email: userEmail,image:userImage };
+const addUserToDB = (userName, userPass, userEmail, userImage) => {
+  const user_ = {
+    username: userName,
+    password: userPass,
+    email: userEmail,
+    image: userImage,
+  };
   const user = new User(user_, (err) => {
     if (err) {
       console.log("schema doesnt match");
@@ -71,13 +76,18 @@ const addUserToDB = (userName, userPass, userEmail,userImage) => {
 //============
 
 //regitster post route
-router.post("/register",upload.single('file'), async (req, res) => {
+router.post("/register", upload.single("file"), async (req, res) => {
   //const username = req.body;
-  
+
   const user = req.body;
   const file = req.file;
-  if (file.detectedFileExtension!=='.jpg') next(new Error("invalid file type")) ;
-  const fileName = Date.now()+user.name + file.detectedFileExtension;
+  if (
+    file.detectedFileExtension !== ".jpg" ||
+    file.detectedFileExtension !== ".PNG"
+  ) {
+    // next(new Error("invalid file type"));
+  }
+  const fileName = Date.now() + user.name + file.detectedFileExtension;
   // fs.mkdir(`${__dirname}/../public/uploads/${user.email}_`, function(err) {
   //   if (err) {
   //     console.log(err)
@@ -85,23 +95,22 @@ router.post("/register",upload.single('file'), async (req, res) => {
   //     console.log("New directory successfully created.")
   //   }
   // })
-  
+
   // const urlFile=fileUrlConverter(imagePath);
-  
-  await pipeline(file.stream, fs.createWriteStream(fileName))
-  
+
+  await pipeline(
+    file.stream,
+    fs.createWriteStream(`./public/uploads/${fileName}`)
+  );
+
   const result = await checkUserExist(user.email);
-  
 
   if (result !== null || result > 0) {
     res.send("A user with this email address already exists");
   } else {
-  
-      addUserToDB(user.name, user.password, user.email,fileName);
+    addUserToDB(user.name, user.password, user.email, fileName);
     res.send("user in");
   }
-    
-  
 });
 
 //Login route
@@ -111,11 +120,12 @@ router.post("/login", async (req, res) => {
   const user = req.body;
   const result = await checkUserCredMatch(user.email, user.password);
   const name = result._doc.username;
+  console.log(name);
   //path for image
   const imagepath = result._doc.image;
   if ((result !== null) | (result > 0)) {
     //here the image path is in a file:// format what should i do instead?
-    const answer = { answer: true, name: name, image: imagepath};
+    const answer = { answer: true, name: name, image: imagepath };
     res.send(answer);
   } else {
     res.send("Wrong email or password");
